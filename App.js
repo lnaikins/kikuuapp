f73a844d2052d0455762dd294c107c1e0e2692b7
lnaikins <lizamimi7@gmail.com>
2023-09-09 16:23:45 -0700

    bottom nav bar

---
+++ App.js
@@ -0,0 +1,13 @@
+import * as React from 'react';
+import MainContainer from './Navigation/MainContainer' ;
+import { Appearance } from 'react-native';
+
+
+function App() {
+  return(
+    <MainContainer/>
+  );
+}
+
+
+export default App;
---
+++ Navigation/MainContainer.js
@@ -0,0 +1,53 @@
+import * as React from 'react';
+
+import { NavigationContainer } from '@react-navigation/native'; 
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Ionicons from 'react-native-vector-icons/Ionicons'
+
+//Screens
+import HomeScreen from './Screens/HomeScreen'
+import DetailsScreen from './Screens/DetailsScreen'
+import SettingsScreen from './Screens/SettingsScreen' 
+
+//Screen Names
+const homeName = 'Home';
+const detailsName = 'Details';
+const settingsName = 'Settings';
+
+const Tab = createBottomTabNavigator();
+
+export default function MainContainer(){
+    return(
+      <NavigationContainer>
+        <Tab.Navigator 
+        initialRouteName={homeName}
+        screenOptions={({route}) => ({
+            tabBarIcon: ({focused, color, size}) => {
+                let iconName;
+                let rn = route.name;
+
+                if (rn === homeName) {
+                    iconName = focused ? 'home' : 'home-outline'
+                } else if (rn === detailsName) {
+                    iconName = focused ? 'list' : 'list-outline'
+                } else if (rn === settingsName) {
+                    iconName = focused ? 'settings' : 'settings-outline';
+                }
+
+                return <Ionicons name={iconName} size={size} color={color}/>
+            },
+        })}
+        tabBarOptions={{
+            activeTintColor: '#FF99C8'
+        }}
+        >
+
+            <Tab.Screen name={homeName} component={HomeScreen} />
+            <Tab.Screen name={detailsName} component={DetailsScreen} />
+            <Tab.Screen name={settingsName} component={SettingsScreen} />
+
+
+        </Tab.Navigator>
+      </NavigationContainer>
+    );
+}
