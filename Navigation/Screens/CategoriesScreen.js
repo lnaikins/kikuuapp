005be838d3e9af6ddf366d5af2c673705ea1435d
Isaac Akakpo <isaacakakpo4@gmail.com>
2023-10-01 19:55:35 +0000

    Moment

--- Navigation/Screens/CategoriesScreen.js
+++ Navigation/Screens/CategoriesScreen.js
@@ -110,6 +110,9 @@ export default function CategoriesScreen({ navigation }) {
                         keyExtractor={(item) => item.id.toString()}
                         numColumns={2} // Change this value to adjust the number of columns
                     />
+
+
+
                 </View>
             </View>
         </View>
