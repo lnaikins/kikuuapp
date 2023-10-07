3fe6052ce5675f59f6b5506687d03b0259b99ce0
Isaac Akakpo <isaacakakpo4@gmail.com>
2023-10-07 20:02:46 +0000

    Added Switch to Setttings Page

--- Navigation/Screens/SettingsScreen.js
+++ Navigation/Screens/SettingsScreen.js
@@ -1,17 +1,33 @@
 import * as React from "react";
-import { View, Text, StyleSheet, useColorScheme } from "react-native";
+import {useState} from 'react';
+import { View, Text,Switch, StyleSheet, useColorScheme } from "react-native";
 
 export default function SettingsScreen({ navigation }) {
   const isDarkMode = useColorScheme() === "dark";
 
+  const [isEnabled, setIsEnabled] = useState(false);
+
   return (
-    <View style={isDarkMode ? styles.blackcontainer : styles.whitecontainer}>
-      <Text style={isDarkMode ? styles.whiteText : styles.darkText}>
-        Hello World
-      </Text>
-    </View>
-  );
+    <View style={styles.container}>
+      <Text style={styles.textStyle}>Dark Theme</Text>
+      <Switch
+        trackColor={{false: '#767577', true: '#81b0ff'}}
+        thumbColor={isEnabled ? '#f5dd4b' : '#f4f3f4'}
+        ios_backgroundColor="#000000"
+        onValueChange={ (previousState) => 
+          setIsEnabled(previousState)
+        }
+        value={isEnabled}
+      />
+    </View>)
+      // <View style={isDarkMode ? styles.blackcontainer : styles.whitecontainer}>
+      // <Text style={isDarkMode ? styles.whiteText : styles.darkText}>
+      //   Hello World
+      // </Text>
+      // </View>
 }
+
+
 
 const styles = StyleSheet.create({
   blackcontainer: {
@@ -30,4 +46,17 @@ const styles = StyleSheet.create({
   darkText: {
     color: "#181818",
   },
+  container: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    margin : 20,
+    justifyContent: 'center',
+  },
+  textStyle : {
+    marginRight : 10
+  }
 });
+
+
+
+
